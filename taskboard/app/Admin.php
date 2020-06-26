1806ff85e3830b5062b0adf429e4063d26a069a9
bwall <brendan.wall03@gmail.com>
2020-06-26 22:16:43 +0100

    Added multiAuthentication Admin and users login

--- taskboard/app/Admin.php
+++ taskboard/app/Admin.php
@@ -6,9 +6,10 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use Notifiable;
+    protected $guard ='admin';
 
     /**
      * The attributes that are mass assignable.
